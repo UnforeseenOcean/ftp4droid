5b0b21812b8c46c2eb36a5f9ee46077d1f9da3ed
tao.pei <tao.pei@ck-telecom.com>
2016-08-12 13:45:50 +0800

    Add client folder

---
+++ app/src/main/java/com/ftp4droid/client/ClientMainActivity.java
@@ -0,0 +1,14 @@
+package com.ftp4droid.client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ftp4droid.R;
+
+public class ClientMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_main);
+    }
+}
